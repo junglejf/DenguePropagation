8179b2d3dabccd04474497553c3569c6ab7a6a9b
junglejf <jfcchagas@id.uff.br>
2019-05-28 17:03:51 -0300

    Gráfico teste inicial só utilizando a população de mosquitos pg55

--- rk.py
+++ rk.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
-
+import matplotlib.pyplot
 import os
 
 # LISTA DE EQUAÇÕES PÁGINAS 44 E 45 (FALTA TERMINAR):
@@ -63,31 +63,33 @@ coef = [1.0, 0.33, 0.14, 0.346, 0.05, 0.05, 0.0167, 0.042, 0.04, 0.059, 0.0, 0.0
 
 
 #Valores iniciais
-iteracoes = 72000
+iteracoes = 24
 
 t = 0
-h = 0.02
+h = 0.04
 
-Et = 100
-Lt = 200
-Pt = 300
-W1t = 100
-W2t = 100
-W3t = 200
+Et = 10
+Lt = 20
+Pt = 100
+W1t = 11
+W2t = 12
+W3t = 13
 Wt = W1t + W2t + W3t
 St = 2
 It = 3 
 Rt = 4
 
-Y0 = [float(Et), float(Lt), float(Pt), float(W1t), float(W2t), float(W3t), float(Wt), float(St), float(It), float(Rt)]
-
+#Y0 = [float(Et), float(Lt), float(Pt), float(W1t), float(W2t), float(W3t), float(St),float(et) float(It), float(Rt)]
+Y0 = [float(Et), float(Lt), float(Pt), float(W1t), float(W2t), float(W3t)]
 
 Cfixo = 700.0     #Valor referente ao periodo favoravel   || Cfixo = 500 -> Intermediário || Cfico = 300 -> desfavorável
-Ci = 0.00000044         #Valor do teorema do chute
+Ci = 0.0014         #Valor do teorema do chute
 Clinha = Cfixo * Ci     #capacidade de suporte ambiental
 
 N = 500000.0 #Populacao humana
 
+YN = [] # Solucoes
+TN = []
 
 ##############################
 ######## Funcoes #############
@@ -128,19 +130,23 @@ def kutta(yk, h, f):
     return somaVetor(yk, v1)
 
 def f(t, y):
+# Y0 = [float(Et), float(Lt), float(Pt), float(W1t), float(W2t), float(W3t), float(St),float(et) , float(It), float(Rt)]
+#           y[0] ,    y[1]  ,  y[2]    ,  y[3]     ,    y[4]   , y[5]      ,  y[6]    ,   y[7]   ,  y[8]    ,  y[9]                
     
-
-    f1 = coef[0] * (1.0 - (y[0] / Clinha)) * y[6]  - (coef[1] + coef[4]) * y[0]        # Página 44, equação E(t)
+    ## Mosquito ##
+    f1 = coef[0] * (1.0 - (y[0] / Clinha)) * ( (y[3]+y[4]+y[5])  - (coef[1] + coef[4]) * y[0] )       # Página 44, equação E(t)
     f2 = coef[1] * y[0] - (coef[2] + coef[5] + coef[10]) * y[1]         # Página 44, equação L(t)
     f3 = coef[2] * y[1]  - (coef[3] + coef[6] + coef[11]) * y[2]         # Página 44, equação P(t)
-    f4 = coef[3] * y[2] - (coef[13] * (y[8]  / N) + coef[7] + coef[12]) * y[3]         # Página 44, equação W1(t)
-    f5 = coef[13] * (y[8]  / N) * y[3]  - (coef[15] + coef[7] + coef[12]) * y[4]         # Página 44, equação W2(t)
+    f4 = coef[3] * y[2] - (coef[13] * (0  / N) + coef[7] + coef[12]) * y[3]         # Página 44, equação W1(t)
+    f5 = coef[13] * (0  / N) * y[3]  - (coef[15] + coef[7] + coef[12]) * y[4]         # Página 44, equação W2(t)
     f6 = coef[15] * y[4]  - (coef[7] + coef[12]) * y[5]         # Página 44, equação W3(t)
-    f7 = coef[19] * N - (coef[14] * (y[5]  / y[6] ) + coef[18]) * y[7]        # Página 45, equação s(t)
-    f8 = coef[14] * (y[5]  / y[6] ) * y[7]  - (coef[16] + coef[18]) * y[0]       # Página 45, equação e(t)
-    f9 = coef[16] * y[0]  - (coef[17] + coef[18]) * y[8]         # Página 45, equação i(t)
-    f10 = coef[17] * y[8]  - coef[18] * y[9]         # Página 45, equação r(t)
-    yFa = [f1,f2, f3, f4, f5, f6, f7, f8, f9, f10]
+    
+    ## Humanos ##
+    #f7 = coef[19] * N - (coef[14] * (y[5]  / y[6] ) + coef[18]) * y[7]        # Página 45, equação s(t)
+    #f8 = coef[14] * (y[5]  / y[6] ) * y[7]  - (coef[16] + coef[18]) * y[0]       # Página 45, equação e(t)
+    #f9 = coef[16] * y[0]  - (coef[17] + coef[18]) * y[8]         # Página 45, equação i(t)
+    #f10 = coef[17] * y[8]  - coef[18] * y[9]         # Página 45, equação r(t)
+    yFa = [f1,f2, f3, f4, f5, f6]
     return yFa
 
 
@@ -173,6 +179,7 @@ def kutta_exp (yk, fk, h, y0, f0, t, x):
 #Metodo explicito iterativo
 #YK Atual, FK Atual, H, Y anterior, F anterior
 def kutta_exp_iter (yk, fk, h, y0, f0, t, iteracoes):
+    global YN
     for i in range(0,iteracoes):
         y1 = escalarXvetor(3,fk)
         y2 = subvetor(y1,f0)
@@ -180,19 +187,44 @@ def kutta_exp_iter (yk, fk, h, y0, f0, t, iteracoes):
         yn = somaVetor(yk,y3)
         print("Y"+ str(i) + ":")
         print(yn)
+        print("t = "+str(t))
         write_file(yn)
+        YN.append(yn)
+        TN.append(i)
         t = t+h
         fk = f(t,yn)
         y0 = yk
         f0 = f(t,yk)
         yk = yn
 
+# Seleciona conjunto de variáveis do array e soma
+#                            [Y0]               [Y1]                [Y2]
+# Exemplo : YN = [ [x(1) , y(1) , z(1)] , [x(2), y(2),z(2)], [x(n),y(n),z(n)] ] & TN = [1,2,3,4, ....]
+# indicesYN = array de Boolean com 1 nas equações que desejamos usar
+def filtrar_variavel (YN,indicesYN):
+    filtro = []
+    for i in range(0,len(YN)): # para cada yn que foi adicionado em YN, ou seja, Y0, Y1, Y2, ...., YN
+        soma = 0
+        for j in range(0,len(YN[0]) ): # itera sobre as variáveis de YN, ou seja, x,y,z do exempl oacima
+            if (indicesYN[j] == 1 ):
+                soma += YN[i][j]
+        filtro.append(soma)
+    
+    return filtro
+
+# Função que plotta um gráfico
+def desenha_grafico (titulo,eixoX, eixoY, nome_eixoX, nome_eixoY):
+    matplotlib.pyplot.title(titulo)
+    matplotlib.pyplot.plot(eixoX, eixoY)
+    matplotlib.pyplot.ylabel(nome_eixoY)
+    matplotlib.pyplot.xlabel(nome_eixoX)
+    matplotlib.pyplot.show()
 
 ##############################
 ######## Programa ############
 ##############################
 
-remove_file()
+#remove_file()
 
 #Com um passo eu resolvo range Kutta de segunda ordem
 Y1 = range_kutta(Y0, h, t)
@@ -208,3 +240,10 @@ print(Y1)
 # Método explícito iterativo!
 iterkutta = kutta_exp_iter(Y1, f(t, Y1), h, Y0, f(t, Y0), t, iteracoes)
 
+## GRÁFICO ##
+filtro = [0,0,0,1,1,1] # selecionar w1,w2,w3
+eixoY = filtrar_variavel(YN,filtro) # <--- W(t)
+titulo = ('Populacao Mosquitos Adultos num periodo de 24h')
+nome_eixoY = ('Quantidade de Mosquito Adulto')
+nome_eixoX = ('Tempo em hora')
+desenha_grafico (titulo,TN, eixoY, nome_eixoX, nome_eixoY )
